a19c452cd0e4dd5105dd6384de671695667ef133
Josh Clifford <joshua.clifford91@gmail.com>
2016-02-07 02:06:18 -0600

    Doing some refactoring. Updated the UI a bit. Should probably find a better way to align things than PadRight...

--- BoardFootCalculator/Program.cs
+++ BoardFootCalculator/Program.cs
@@ -1,53 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 namespace BoardFootCalculator
 {
     internal class Program
     {
-        private static List<double> _stockLengths;
-
         private static void Main()
         {
-            _stockLengths = AppSettingsHelper.GetStockLengths();
-
             var cutList = CutRetreiver.GetCuts();
-            var groupedCuts = new List<double>();
-            var stockCounts = _stockLengths.ToDictionary(x => x, x => new List<List<double>>());
+            var stockCounts = CalculateMinimumStock(ref cutList);
 
+            PrintResults(stockCounts, cutList);
+            Console.ReadKey();
+        }
+
+        private static Dictionary<double,List<List<double>>> CalculateMinimumStock(ref List<double> cutList)
+        {
+            var stockLengths = AppSettingsHelper.GetStockLengths();
+            var stockCounts = stockLengths.ToDictionary(x => x, x => new List<List<double>>());
+            var groupedCuts = new List<double>();
+
+            // While we still have cuts that haven't been grouped, keep churning.
             while (cutList.Count > 0)
             {
+                // We always want to grab the longest cuts first to "fill in the gaps" with shorter cuts.
                 cutList = cutList.OrderByDescending(x => x).ToList();
-                foreach(var cut in cutList)
+                foreach (var cut in cutList)
                 {
-                    if (groupedCuts.Sum(x => x) + cut <= _stockLengths.Max())
+                    if (groupedCuts.Sum(x => x) + cut <= stockLengths.Max())
                     {
                         groupedCuts.Add(cut);
                     }
                 }
 
+                // No cuts were short enough to be added to the group.
+                // At this point, we're left with boards that are too long to fit the stock sizes.
+                // Bail out of the loop.
                 if (groupedCuts.Count == 0) break;
 
-                foreach(var cutToRemove in groupedCuts)
+                foreach (var cutToRemove in groupedCuts)
                 {
                     cutList.Remove(cutToRemove);
                 }
 
-                foreach(var stockLength in _stockLengths.OrderBy(x => x))
+                // Check the total length of the grouped cuts to see what is the smallest board we can get away with using.
+                foreach (var stockLength in stockLengths.OrderBy(x => x))
                 {
+                    // If we match on a board, don't keep checking larger boards!
                     if (groupedCuts.Sum(x => x) <= stockLength)
                     {
                         stockCounts[stockLength].Add(new List<double>(groupedCuts));
+                        break;
                     }
                 }
 
                 groupedCuts.Clear();
             }
 
-            PrintResults(stockCounts, cutList);
-            Console.ReadKey();
+            return stockCounts;
         }
 
         private static void PrintResults(Dictionary<double, List<List<double>>> results, List<double> cutsRemaining)
@@ -56,13 +67,14 @@ namespace BoardFootCalculator
             {
                 if (result.Value.Any())
                 {
-                    var headerString = $"Length: {result.Key}inches, Quantity: {result.Value.Count}";
+                    var headerString = $"Length: {result.Key}\", Quantity: {result.Value.Count}";
                     Console.WriteLine(headerString);
                     Console.WriteLine(new string('-', headerString.Length));
 
                     foreach (var group in result.Value)
                     {
-                        Console.WriteLine(string.Join(",", group));
+                        Console.Write(string.Join(",", group.Select(x => $"{x}\"")).PadRight(30));
+                        Console.WriteLine($"Waste: {result.Key - group.Sum(x => x)}\"");
                     }
 
                     Console.WriteLine("");
